05c8c47d917ac6d04d50ff7fefb1dbb653874ba4
dapinderbhullar <dpinderbhullar@gmail.com>
2026-03-24 15:32:57 +0530

    added transformation logic

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,19 @@
-print("Pipeline started")
-print("Processing data...")
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Ecommerce Pipeline").getOrCreate()
+
+print("Pipeline started...")
+
+df = spark.read.csv("dataset_ecommerce/olist_orders_dataset.csv", header=True)
+
+print("Data Loaded")
+
+# Basic transformation
+status_count = df.groupBy("order_status").count()
+
+print("Order Status Summary:")
+status_count.show()
+
+spark.stop()
+
 print("Pipeline completed")
